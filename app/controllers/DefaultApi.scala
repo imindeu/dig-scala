68eb10c21f4bc0e227cf383b9d4cd25ca1f0f177
Laszlo Vass <laszlo.vass@imind.eu>
2014-12-12 22:31:18 +0100

    ws.url fix2

--- app/controllers/DefaultApi.scala
+++ app/controllers/DefaultApi.scala
@@ -5,9 +5,11 @@ import play.api._
 import play.api.mvc._
 import models._
 import play.api.db.DB
-import play.api.libs.ws.WS
+import play.api.libs.ws.{Response, WS}
 import play.api.libs.json.Json
 import services.AggregatorService
+import scala.concurrent.{ExecutionContext, Future}
+import ExecutionContext.Implicits.global
 
 /**
  * Created by vassdoki on 12/12/14.
@@ -25,7 +27,7 @@ object DefaultApi extends Controller {
                   EventData.persist(eventId, d._1, d._2)
                 }
               }
-              WS.url("http://127.0.0.1:9000/aggregator").post(Json.stringify(Json.obj("id" -> eventId)))
+              WS.url("http://127.0.0.1:9000/aggregator").withHeaders(("Content-Type", "text/json")).post(Json.stringify(Json.obj("id" -> eventId)))
               Ok("Stored")
           }.getOrElse(BadRequest("Event not stored"))
         }
@@ -40,7 +42,7 @@ object DefaultApi extends Controller {
           Event.persist(user.id.get).map {
             eventId => {
               EventData.persist(eventId, AggregatorService.HyperCounter, (request.body \ "name").toString())
-              WS.url("http://127.0.0.1:9000/aggregator").post(Json.stringify(Json.obj("id" -> eventId)))
+              WS.url("http://127.0.0.1:9000/aggregator").withHeaders(("Content-Type", "text/json")).post(Json.stringify(Json.obj("id" -> eventId)))
               Ok("Stored")
             }
           }.getOrElse(BadRequest("Event not stored"))
